3e152130557e38cd8f95764dfec5db44323873bb
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2023-12-12 19:22:40 +0545

    Add files via upload
    .Introduction to JavaScript:
    
    JavaScript is a scripting language primarily used for building web applications.
    It is a versatile language that supports object-oriented, imperative, and functional programming styles.
    JavaScript is an integral part of web development, providing dynamic and interactive features to websites.
    Hello, World! Program:
    
    The program consists of a single line: console.log("Hello, World!");.
    console.log is a function that outputs the specified message to the console.
    The message "Hello, World!" is a common introductory message used in programming examples.
    .Coding Conversion:
    
    JavaScript is typically embedded within HTML documents for web development.

---
+++ Lab-Work Js/hello.js
@@ -0,0 +1,23 @@
+//Print Methods 
+//1
+var userInput = window.prompt("Enter a message:");
+console.log(userInput);
+
+//2
+console.log("hello World");
+
+
+
+//Introduction to JavaScript:
+
+//JavaScript is a scripting language primarily used for building web applications.
+//It is a versatile language that supports object-oriented, imperative, and functional programming styles.
+//JavaScript is an integral part of web development, providing dynamic and interactive features to websites.
+//Hello, World! Program:
+
+//The program consists of a single line: console.log("Hello, World!");.
+//console.log is a function that outputs the specified message to the console.
+//The message "Hello, World!" is a common introductory message used in programming examples.
+//Coding Conversion:
+
+//JavaScript is typically embedded within HTML documents for web development.
