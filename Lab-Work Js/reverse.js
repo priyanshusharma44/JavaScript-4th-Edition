66b2a7cafa6c157715db4b84b5de4991cdca78b1
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2023-12-21 19:22:36 +0545

    Update reverse.js

--- Lab-Work Js/reverse.js
+++ Lab-Work Js/reverse.js
@@ -1,11 +1,11 @@
-let n=prompt("Enter A Name:");
-let r="";
-let i=n.length-1;
+let n=prompt("Enter A Name:"); // RAMOS = 5 character and 4 index
+let r=""; // r= blank 
+let i=n.length-1; // i= RAMOS.lenght -1 -> RAMOS = 5 and lenght -1 = 5-1 = 4 index which is S 
 
 do{
-    r=r+n[i];
-    i--;
+    r=r+n[i]; // any how /......,..... r= "" + RAMOS[4] = S ...so intially r = S 
+    i--; // i=3
    
 }
-while(i>=0)
-console.log(r);
+while(i>=0)   // 3>=0 ? 
+console.log(r); // S and repeat same 
