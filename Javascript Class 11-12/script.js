dbb6e467268256c11a202b39a9f7e9190b84e5d0
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2024-02-20 21:47:07 +0545

    Update script.js

--- Javascript Class 11-12/script.js
+++ Javascript Class 11-12/script.js
@@ -1,66 +1,41 @@
-//js theory need ...
-//Js extenstion .js
-//1. Alert Use
+// Day 1: Introduction to JavaScript
+
+// 1. Alert Use
 alert("Are You Sure ?");
 
-//2.
+// 2. Console Log
 console.log("Jay Nepal");
 
-//3. variable (1st from browser and then from here)
-
+// 3. Variables
 fName = "Priyanshu";
 age = 22;
 console.log(fName);
 console.log(age);
 
-//4. Null and undefine
+// 4. Null and Undefined
 x = null;
 y = undefined;
 console.log(x);
 console.log(y);
 
-//5.
+// 5. Boolean
 isFollow = true;
 console.log(isFollow);
 
-//Case Sensitive
-
-//6.Keywords (var,let,const)
-
-//a. Let [Cant be redeclared but can be updated. A Block Scope]
-//b. Var [can be redeclared and can also be updaed. Global Scope]
-//c. Const [cant be redeclared or updated. A block Scope]
-
+// 6. Keywords (var, let, const)
+//    a. Let [Cannot be redeclared but can be updated. A Block Scope]
+//    b. Var [Can be redeclared and updated. Global Scope]
+//    c. Const [Cannot be redeclared or updated. A Block Scope]
 var a = 10;
 var b = 20;
 console.log(b);
 
-//bad way cause memory space is highly used
-//so use let but let cant be redeclared line below
-/*
-let a=10;
-let a=10;
-
-
-
-
-*/
 let agx = 20;
 agx = 21;
 agx = 22;
 console.log(agx);
 
-//const need to given a value and a fixed value . null cant be give
-let m;
-console.log(m);
-//undefined comes
-
-// const av;
-// console.log(av);
-//error comes
-
-//8.
-//block scope and global scope
+// 7. Block Scope and Global Scope
 {
   let xd = 10;
   console.log(a);
@@ -68,57 +43,148 @@ console.log(m);
 let xd = 20;
 console.log(a);
 
-//datatype
+// 8. Data Types
 let ma = 24;
 console.log(ma);
 let xa = typeof ma;
 console.log(xa); //number
-
-//9.number,string,boolean,null etc
 
 let ram = null;
 console.log(ram);
 let mom = typeof ram;
 console.log(mom); // null means absent of an object
 
-//10. Non primitive (Object , Array, Function)
-//object = key value and we use const
-//at last semicolon
+// 9. Non-primitive Data Types (Object, Array, Function)
 const max = {
   name: "Ram",
   roll: 12,
 };
 console.log(max);
 
-//use dot operator for specific value fetching or use []
 console.log(max.name);
-
-//or use
 console.log(max["roll"]);
-
-//if we wanna increase the objects age value
 
 max.roll = max.roll + 1;
 console.log(max);
 
-//or also using
 max["roll"] = max["roll"] + 1;
 console.log(max);
-//specific value
 console.log(max["roll"]);
 
-//operators
-
-//last topic
-
-//and , or  , not ,
-//+ , - , * , /
-
+// Operators
 let s = 10;
 let sy = 20;
-console.log("The Addition of s and sy is  =", s + sy);
-console.log("The Division of s and sy is  =", s / sy);
-console.log("The Mod of s and sy is  =", s % sy);
+console.log("The Addition of s and sy is =", s + sy);
+console.log("The Division of s and sy is =", s / sy);
+console.log("The Mod of s and sy is =", s % sy);
+console.log("The Expo(Power) of s and sy is =", s ** sy);
 
-//Exponential we use ** a**b = a to the power b
-console.log("The Expo(Power) of s and sy is  =", s ** sy);
+// Control Structures and Functions
+
+// Conditional Statements
+if (s < sy) {
+  console.log("s is less than sy");
+} else {
+  console.log("s is greater than sy");
+}
+
+// Loops
+for (let i = 0; i < 5; i++) {
+  console.log("Index:", i);
+}
+
+// Do-while loop
+let j = 0;
+do {
+  console.log("Do-While Index:", j);
+  j++;
+} while (j < 5);
+
+// While loop
+let k = 0;
+while (k < 5) {
+  console.log("While Index:", k);
+  k++;
+}
+
+// Functions
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Alice");
+
+// Function to calculate the area of a rectangle
+function calculateArea(length, width) {
+  return length * width;
+}
+
+// Calling the function and storing the result in a variable
+let area = calculateArea(5, 3);
+console.log("Area of the rectangle:", area);
+
+// Object-Oriented Programming Concepts and Event Handling
+
+// Objects
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+console.log(person.fullName());
+
+// Event Handling
+document.getElementById("myButton").addEventListener("click", function () {
+  console.log("Button clicked!");
+});
+
+// Day 2: Intermediate JavaScript and Introduction to PHP
+// (Continuation within the same file)
+
+// Dive Deeper into JavaScript
+
+// Arrays
+const colors = ["red", "green", "blue"];
+console.log(colors);
+
+// Example 1: Using push() to add elements to the array
+colors.push("yellow", "orange");
+console.log(colors); // Output: ["red", "green", "blue", "yellow", "orange"]
+
+// Example 2: Using pop() to remove the last element from the array
+let removedColor = colors.pop();
+console.log("Removed Color:", removedColor); // Output: "orange"
+console.log(colors); // Output: ["red", "green", "blue", "yellow"]
+
+// DOM Manipulation
+document.getElementById("myDiv").innerHTML = "Modified Text";
+
+// Example 1: Changing the text content of an element
+document.getElementById("myHeading").textContent = "New Heading Text";
+
+// Example 2: Adding a new element to the DOM
+let newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a new paragraph.";
+
+document.getElementById("myDiv").appendChild(newParagraph);
+
+// Example 3: Removing an element from the DOM
+let elementToRemove = document.getElementById("myElementToRemove");
+elementToRemove.parentNode.removeChild(elementToRemove);
+
+/*
+In Example 1, the textContent property of an element with the id "myHeading" is modified to change its text content to "New Heading Text".
+
+In Example 2, a new <p> element is created using document.createElement(), and its text content is set. Then, the new paragraph element is appended as a child of an element with the id "myDiv".
+
+In Example 3, an element with the id "myElementToRemove" is removed from the DOM by selecting its parent node and then using the removeChild() method to remove the specified child element.
+
+*/
+
+// Intermediate JavaScript ends here
+
+// Introduction to PHP
+// (PHP code snippets can be added in PHP files, not directly within JavaScript)
