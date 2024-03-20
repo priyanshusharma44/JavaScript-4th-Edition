73d7b9c9d9f43356624e9a7ab37032baec6b1031
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2024-03-20 14:08:58 +0545

    Add files via upload

---
+++ Lab-Work Js/lab18/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+echo "<p>Copyright &copy; 2024-" . date("Y")</p>";
+</body>
+</html>
